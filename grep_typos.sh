7331acc942ee1bad54059073b8c3fae7b81213ab
per1234 <accounts@perglass.com>
2018-04-20 20:21:39 -0700

    Add support for whole word matching
    Previously the script would by default provide a match if the typo string was found as a part of a word, leading to false positives. This commit retains the previous behavior when the --substrings option is specified and changes the default behavior of the script to match whole words only.
    
    Each usage has a separate database:
    
    - Whole word matching: data/common_misspellings.txt
      - This file is a mirror of https://en.wikipedia.org/wiki/Wikipedia:Lists_of_common_misspellings/For_machines. Any additions/deletions/corrections should be made to the Wikipedia page and then common_misspellings.txt synced with that page.
    - Substring matching: data/typos.txt
      - This file will be manually curated in this repository.

--- grep_typos.sh
+++ grep_typos.sh
@@ -11,25 +11,45 @@
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied, please provide path."
-    echo "Usage:"
-    echo "$0 path"
-    echo "$0 path --exclude='*.po'"
+    echo "Usage: $0 [OPTION]... PATH"
+    echo "Options:"
+    echo "--exclude=[EXCLUSIONS]    Exclude specified files from processing."
+    echo "--substrings              Match data/typos.txt against substrings. Default behavior is to"
+    echo "                          match data/common_misspellings.txt against whole words only."
     exit 1
 fi
-
-readonly path=$1
 
 # No exclude by default
 exclude_mask=''
 
+# Default to whole word matching
+match_substrings=false
+
 exit_status=0
 
-if [ $# -eq 2 ]
-  then
-  if [[ $2 == --exclude* ]]
-    then
-    exclude_mask=`cut -d "=" -f 2 <<< "$2"`
-  fi
+
+for argument in "$@"
+do
+  case $argument in
+    --exclude=*)
+    exclude_mask=`cut -d "=" -f 2 <<< "$argument"`
+    shift
+    ;;
+    --substrings)
+    match_substrings=true
+    shift
+    ;;
+    *)
+    path=$argument
+    ;;
+  esac
+done
+
+
+if [ "$match_substrings" == true ];then
+  data_file="data/typos.txt"
+else
+  data_file="data/common_misspellings.txt"
 fi
 
 
@@ -38,7 +58,7 @@ wrong_words=()
 while IFS= read -r line
 do
 	wrong_words+=("$line")
-done < "data/typos.txt"
+done < "$data_file"
 
 echo "Search typos in $path"
 
@@ -49,7 +69,11 @@ pattern=""
 for i in "${wrong_words[@]}"
 do
   if [ "$counter" -eq "0" ];then 
-    pattern=$i
+    if [ "$match_substrings" == true ];then
+      pattern=$i
+    else
+      pattern="\b${i}\b"
+    fi
   elif [ "$counter" -eq "$pattern_size" ];then
     if grep --color -r --exclude=$exclude_mask -I "$pattern" $path; then
       exit_status=1
@@ -57,7 +81,11 @@ do
     pattern=$i
     counter=0
   else
-    pattern=$pattern"\|"$i
+    if [ "$match_substrings" == true ];then
+      pattern=$pattern"\|"$i
+    else
+      pattern="${pattern}\\|\b${i}\b"
+    fi
   fi
   counter=$((counter+1))
 done
--- test.sh
+++ test.sh
@@ -3,8 +3,28 @@
 # [Author] Semen Zhydenko
 # ------------------------------------------------------------------
 
+# Part 1, test whole word matching
 tmp_file=$(mktemp)
-./grep_typos.sh . >> $tmp_file
+./grep_typos.sh ./data/common_misspellings.txt >> $tmp_file
+count_from_grep=$(wc -l < $tmp_file)
+count_from_file=$(wc -l < ./data/common_misspellings.txt)
+#rm -rf $tmp_file
+
+# always one more string "Search typos in $path"
+tests_total=$((count_from_file+1))
+
+if [ "$count_from_grep" -eq "$tests_total" ];then
+  echo "Tests passed.";
+  exit 0;
+else
+  echo "Test of whole word matching failed. count_from_grep=$count_from_grep,count_from_file=$count_from_file";
+  exit 1;  
+fi
+
+
+# Part 2, test --substrings
+tmp_file=$(mktemp)
+./grep_typos.sh --substrings ./data/typos.txt >> $tmp_file
 count_from_grep=$(wc -l < $tmp_file)
 count_from_file=$(wc -l < ./data/typos.txt)
 rm -rf $tmp_file
@@ -16,11 +36,12 @@ if [ "$count_from_grep" -eq "$tests_total" ];then
   echo "Tests passed.";
   exit 0;
 else
-  echo "Tests faild. count_from_grep=$count_from_grep,count_from_file=$count_from_file";
+  echo "Test of --substrings failed. count_from_grep=$count_from_grep,count_from_file=$count_from_file";
   exit 1;  
 fi
 
-# Part 2, testing exclude
+
+# Part 3, testing exclude
 tmp_file=$(mktemp)
 ./grep_typos.sh . --exclude='*.txt' >> $tmp_file
 count_from_grep=$(wc -l < $tmp_file)
@@ -30,6 +51,6 @@ if [ "$count_from_grep" -eq 0 ];then
   echo "Tests passed.";
   exit 0;
 else
-  echo "Tests faild. count_from_grep=$count_from_grep, but should be 0";
+  echo "Test of --exclude failed. count_from_grep=$count_from_grep, but should be 0";
   exit 1;  
 fi
